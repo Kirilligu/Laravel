3ceba5c2fcb96b1f6c7e51ac2a672b6d473a6139
Kirilligu <k.degtyarev4@mail.ru>
2024-11-03 01:16:58 +0800

    Создание формы и контроллера для обработки данных

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
+
+Route::get('/', [FormController::class, 'showForm'])->name('form.show');
+Route::post('/submit', [FormController::class, 'submitForm'])->name('form.submit');
+Route::get('/data', [FormController::class, 'showData'])->name('data.show');
